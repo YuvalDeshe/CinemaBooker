912c53bf04b9240168688aa742d1f676d2127b24
Sebastian Geer <sebgeer03@gmail.com>
2025-09-26 12:59:32 -0400

    Merging changes
    Merging into Branch

--- cinema-booker/.next/server/server-reference-manifest.js
+++ cinema-booker/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QTX0PoIZ6cPlYqkWuQ7gNQaAvsJZ9RdjHTzEsIhRlVs=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4CqdwJ3Qb26nnOCI+iQGnRgdnsPGkEOEbYSsxIjECEM=\"\n}"
