00947fe6e8e9c8065bc4f87b0332e92a6157758e
Yuval Deshe <yuvaldeshe@gmail.com>
2025-09-28 11:24:11 -0400

    Clean up unnecessary comments

--- cinema-booker/src/app/movie/[id]/page.tsx
+++ cinema-booker/src/app/movie/[id]/page.tsx
@@ -8,12 +8,6 @@ import styles from "./styles.module.css"
 import { useRouter, useParams } from "next/navigation";
 import Link from "next/link";
 
-/** 
-  NOTE: This page is located under the URL of movie/MOVIE_ID
-  When the DB is implemented, have the movie's ID (or name or something else)
-  replace "MOVIE_ID" (with "[id]" if I'm not mistaken) in the URL so that movies 
-  can be loaded dynamically.
-*/ 
 
 //Array of hardcoded showtimes, loaded into the page dynamically.
 const SHOWTIMES = ["9:00 a.m.", "12:00 p.m.", "3:00 p.m.", "6:00 p.m.", "9:00 p.m."]
@@ -33,28 +27,6 @@ type Movie = {
   _id: string;
 }
 
-/**
-  This is the movie object. Once we have the DB up and running, replace the below fields with relevant info.
-  Feel free to replace if you want, but currently, the webpage reads the info directly from
-  this object called "movie".
-*/
- /**  const movie: Movie = {
-  title: "Movie Title",
-  genre: "GENRE",
-  description: "This is the description for the movie. Lorem ipsum dolor sit amet, \
-  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore \
-  magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
-  ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
-  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat \
-  non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  posterImgUrl: "https://www.nextdayflyers.com/blog/wp-content/uploads/2012/02/drive.jpg",
-  trailerLink: "https://www.youtube.com/watch?v=MtN1YnoL46Q",
-  director: "Director Information",
-  castList: ["Actor1", "Actor2", "Actor3", "Actor4", "Actor5"],
-  rating: "RATING",
-  runtime: "RUNTIME" 
-} 
-*/
 export default function MoviePage() {
   const { id } = useParams();
   const router = useRouter();
