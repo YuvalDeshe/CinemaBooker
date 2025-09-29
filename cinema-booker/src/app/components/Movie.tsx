9146add314ff3df533870c46f14b3680ae0ca150
Yuval Deshe <yuvaldeshe@gmail.com>
2025-09-28 21:03:33 -0400

    Add showtime buttons to Movies on the home page.

--- cinema-booker/src/app/components/Movie.tsx
+++ cinema-booker/src/app/components/Movie.tsx
@@ -1,13 +1,23 @@
+'use client';
+
+import {useRouter} from "next/navigation";
 import React from "react";
+import styles from "./styles.module.css";
 
 type MovieProps = {
+  _id: string;
   title: string;
   genre: string[];
   posterUrl?: string;
-  isCurrentlyRunning?: boolean;
+  isCurrentlyRunning: boolean;
 };
 
-export default function Movie({ title, genre, posterUrl, isCurrentlyRunning}: MovieProps) {
+// if we ever have to have individual showtimes per movie, replace this with data from MongoDB
+const SHOWTIMES = ["9:00 a.m.", "12:00 p.m.", "3:00 p.m.", "6:00 p.m.", "9:00 p.m."]
+
+export default function Movie({ _id, title, genre, posterUrl, isCurrentlyRunning }: MovieProps) {
+  const router = useRouter();
+
   return (
     <div className="border rounded shadow p-4 bg-gray-600 flex flex-col items-center w-48">
       <div className="w-32 h-48 bg-gray-200 flex items-center justify-center mb-2">
@@ -19,6 +29,31 @@ export default function Movie({ title, genre, posterUrl, isCurrentlyRunning}: Mo
       </div>
       <h3 className="font-semibold text-lg mb-1 text-center">{title}</h3>
       <div className="text-xs text-white text-center">{genre.join(", ")}</div>
+      <div className="flex flex-col gap-2 w-full mt-4 mb-4">
+        <a
+          href={`/movie/${_id}`}
+          className="w-full"
+        >
+          <button
+            className="w-full px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800 transition"
+            type="button"
+          >
+            Movie Details
+          </button>
+        </a>
+      </div>
+      {isCurrentlyRunning && (<div className="text-cs text-white text-center">
+        <h3>Showtimes:</h3>
+        {SHOWTIMES.map((value, index) => (
+          <button
+            className={styles.showTimeButtons}
+            onClick={() => router.push(`/movie/${_id}/booking?time=${encodeURIComponent(value)}`)}
+            key={`${title} ${index}`}
+          >
+            {value}
+          </button>
+        ))}
+      </div>)}
     </div>
   );
 }
--- cinema-booker/src/app/components/MovieList.tsx
+++ cinema-booker/src/app/components/MovieList.tsx
@@ -6,7 +6,7 @@ type MovieType = {
   title: string;
   genre: string[];
   posterUrl?: string;
-  isCurrentlyRunning?: boolean;
+  isCurrentlyRunning: boolean;
   _id: string;
 };
 
@@ -17,35 +17,36 @@ type MovieListProps = {
 export default function MovieList({ movies }: MovieListProps) {
   return (
     <section className="w-full flex flex-wrap gap-8 justify-center mt-8">
-      {movies.map((movie) => (
-        <div
-          className="movie-card flex flex-col items-center bg-gray-800 rounded-lg shadow-lg p-4 w-64"
-          key={movie._id}
-        >
-          <img
-            src={movie.posterUrl}
-            alt={movie.title}
-            className="mb-2 w-40 h-60 object-cover rounded"
-          />
-          <h3 className="text-lg font-bold text-white">{movie.title}</h3>
-          <p className="text-gray-300 text-sm mb-2">
-            {movie.genre.join(", ")}
-          </p>
-          <div className="flex flex-col gap-2 w-full mt-4">
-            <a
-              href={`/movie/${movie._id}`}
-              className="w-full"
-            >
-              <button
-                className="w-full px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800 transition"
-                type="button"
-              >
-                Movie Details
-              </button>
-            </a>
-          </div>
-        </div>
-      ))}
+      {movies.map((movie) => <Movie title={movie.title} genre={movie.genre} _id={movie._id} posterUrl={movie.posterUrl} isCurrentlyRunning={movie.isCurrentlyRunning} key={movie._id}></Movie>)}
     </section>
   );
 }
+// (
+//         <div
+//           className="movie-card flex flex-col items-center bg-gray-800 rounded-lg shadow-lg p-4 w-64"
+//           key={movie._id}
+//         >
+//           <img
+//             src={movie.posterUrl}
+//             alt={movie.title}
+//             className="mb-2 w-40 h-60 object-cover rounded"
+//           />
+//           <h3 className="text-lg font-bold text-white">{movie.title}</h3>
+//           <p className="text-gray-300 text-sm mb-2">
+//             {movie.genre.join(", ")}
+//           </p>
+//           <div className="flex flex-col gap-2 w-full mt-4">
+//             <a
+//               href={`/movie/${movie._id}`}
+//               className="w-full"
+//             >
+//               <button
+//                 className="w-full px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800 transition"
+//                 type="button"
+//               >
+//                 Movie Details
+//               </button>
+//             </a>
+//           </div>
+//         </div>
+//       )
