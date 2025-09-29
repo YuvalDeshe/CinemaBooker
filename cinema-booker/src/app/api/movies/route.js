1597099d3e9b102511861dd642e726f571bb98ed
parkeroutlaw <parker353@outlook.com>
2025-09-28 20:08:39 -0400

    Fixed issue with caching in movies/route.js

--- cinema-booker/src/app/api/movies/route.js
+++ cinema-booker/src/app/api/movies/route.js
@@ -1,18 +1,31 @@
 import { MongoClient } from 'mongodb';
 
 const uri = "mongodb+srv://parkertheoutlaw_db_user:FC6qKAalpje0bIUU@cluster0.levqaeh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri);
+
+let client;
+let db;
+
+async function connectToDatabase() {
+    if (db) {
+        return { db };
+    }
+
+    client = new MongoClient(uri);
+    await client.connect();
+    db = client.db('MoviesDatabase');
+
+    return { db };
+}
 
 export async function GET() {
     try {
-        await client.connect();
-        const db = client.db('MoviesDatabase');
+        const { db } = await connectToDatabase();
         const moviesCollection = db.collection('MoviesCollection');
 
         const movies = await moviesCollection.find({}).toArray();
 
         const formattedMovies = movies.map(movie => ({
-            _id: movie._id, 
+            _id: movie._id,
             title: movie.title,
             genre: Array.isArray(movie.genre)
                 ? movie.genre.map(g => g.trim())
@@ -43,7 +56,5 @@ export async function GET() {
                 'Content-Type': 'application/json',
             },
         });
-    } finally {
-        await client.close();
     }
 }
