8f519524b750826b6415e04dad01eb92d7f6bbe7
Yuval Deshe <yuvaldeshe@gmail.com>
2025-10-22 18:19:50 -0400

    Remove plaintext MongoDB URI for security

--- cinema-booker/src/app/api/users/route.js
+++ cinema-booker/src/app/api/users/route.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 import bcrypt from 'bcryptjs';
 import Status from '@/types/User';
 
-const uri = "mongodb+srv://parkertheoutlaw_db_user:FC6qKAalpje0bIUU@cluster0.levqaeh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 const SALT_ROUNDS = 10;
 
 let client;
