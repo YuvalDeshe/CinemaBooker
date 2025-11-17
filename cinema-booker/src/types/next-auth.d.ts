24007284a4c328a2b5dfdbdce9bfcb94ef267a54
Yuval Deshe <yuvaldeshe@gmail.com>
2025-11-17 16:14:41 -0500

    Add id to session jwt

--- cinema-booker/src/types/next-auth.d.ts
+++ cinema-booker/src/types/next-auth.d.ts
@@ -3,6 +3,7 @@ import { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      id?: string;
       isEmailVerified?: boolean;
       username?: string;
     } & DefaultSession["user"]
